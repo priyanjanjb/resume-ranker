677c9fe8999383a80d9b33faa28e4330cfaf0bff
priyanjanjb <priyanjanjb@gmail.com>
2025-08-18 16:21:49 +0530

    feat(auth): add signup & signin front end logics

--- client/src/components/signIn/signInPage.tsx
+++ client/src/components/signIn/signInPage.tsx
@@ -1,8 +1,33 @@
 import '../../assets/styleSheet/Style.css'
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+
 interface Props {}
 
 function SignInPage(props: Props) {
     const {} = props
+    const navigate = useNavigate();
+    const [loginFormData, setLoginFormData] = useState({
+      email: '',
+      password: ''
+    });
+
+    const handleInputChange =(e: React.ChangeEvent<HTMLInputElement>)=>{
+    const {name , value} = e.target;
+    setLoginFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+      }));
+    }
+
+  const handleSubmit =  async(e: React.FormEvent<HTMLFormElement>) =>{
+    e.preventDefault(); // Prevent page reload
+    console.log("submit ok");
+   navigate('/signup')
+
+    
+  }
+
 
     return (
       <div className="formBackPanel">
@@ -17,14 +42,15 @@ function SignInPage(props: Props) {
             <h1 className="greetingSignIn">Welcome back</h1>
             <div className="formTextSection">
               <p className="actionTopic">Login to your account</p>
-              <form className="formData" >
+              <form className="formData" onSubmit={handleSubmit}>
               <input
                 className="inputBox"
                 type="email"
                 id="email"
                 name="email"
                 placeholder="Email"
-                
+                value={loginFormData.email}
+                onChange={handleInputChange}
                 required
               />
 
@@ -34,6 +60,8 @@ function SignInPage(props: Props) {
                 id="password"
                 name="password"
                 placeholder="Password"
+                value={loginFormData.password}
+                onChange={handleInputChange}
                 required
               />
 
--- client/src/components/signUp/signUpPage.tsx
+++ client/src/components/signUp/signUpPage.tsx
@@ -1,7 +1,39 @@
 import '../../assets/styleSheet/Style.css'
-
+import { useState } from "react";
+import toast from 'react-hot-toast';
+import {useNavigate } from "react-router-dom";
 function SignUpPage() {
-  
+  const navigate = useNavigate();
+  const [signinFormData, setSigninFormData] = useState({
+    fName: '',
+    lName: '',
+    email: '',
+    password: '',
+    cPassword: '',
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setSigninFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const { password, cPassword } = signinFormData;
+
+    if (password !== cPassword) {
+      toast.error("Passwords do not match!");
+      return;
+    }
+    console.log("handle submit");
+    navigate('/');
+
+    
+  }
 
   return (
     <div className='backPanel'>
@@ -18,13 +50,15 @@ function SignUpPage() {
           <div className="formTextSection">
             <p className='actionTopic'>Create your new account</p>
 
-            <form className="signinFormData" >
+            <form className="signinFormData" onSubmit={handleSubmit}>
               <input
                 className='inputBox'
                 type="text"
                 id='fName'
                 name="fName"
                 placeholder='First Name'
+                value={signinFormData.fName}
+                onChange={handleInputChange}
                 
                 required
               />
@@ -34,6 +68,8 @@ function SignUpPage() {
                 id='lName'
                 name="lName"
                 placeholder='Last Name'
+                value={signinFormData.lName}
+                onChange={handleInputChange}
                 required
               />
               <input
@@ -42,6 +78,8 @@ function SignUpPage() {
                 id='email'
                 name="email"
                 placeholder='Email'
+                value={signinFormData.email}
+                onChange={handleInputChange}
                 required
               />
               <input
@@ -50,6 +88,8 @@ function SignUpPage() {
                 id='password'
                 name="password"
                 placeholder='Password'
+                value={signinFormData.password}
+                onChange={handleInputChange}
                 required
               />
               <input
@@ -58,6 +98,8 @@ function SignUpPage() {
                 id='cPassword'
                 name="cPassword"
                 placeholder='Confirm Password'
+                value={signinFormData.cPassword}
+                onChange={handleInputChange}
                 required
               />
 
@@ -66,7 +108,13 @@ function SignUpPage() {
                 <button
                   className='buttons'
                   type='button'
-                  
+                  onClick={() => setSigninFormData({
+                    fName: '',
+                    lName: '',
+                    email: '',
+                    password: '',
+                    cPassword: '',
+                  })}
                 >
                   Reset
                 </button>
